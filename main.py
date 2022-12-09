54cf84261623107911af68e75dbd5048de1e16b1
luisced <luis.cedillo.maldonado@gmail.com>
2022-12-09 13:34:23 -0600

    Finished ai chat

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
+
+margin1, margin2, margin3, margin4, margin5, margin6 = "╔═", "═╗", "╚═", "═╝", "║", "═"
 
 
 def chat_with_gpt3():
     while True:
-        prompt = input("You: ")
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=1024,
-            n=1,
-            stop=None
-        )
+        prompt = input(f"You: ")
+        if prompt == "exit":
+            break
+        else:
+            response = openai.Completion.create(
+                engine="text-davinci-002",
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None
+            )
+            # remove the enter key from the response
+            answer = response['choices'][0]['text'].replace("\n", "")
 
-        print("GPT-3: ", response["choices"][0]["text"])
+        print(f"GPT-3: {answer}")
 
 
 chat_with_gpt3()
-# q: how do i create a virtual environment in python?
